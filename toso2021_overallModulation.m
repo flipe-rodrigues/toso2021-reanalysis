4fe574975118e90d11e240380396669203834dda
flipe rodrigues <filipe.rodrigues@neuro.fchampalimaud.org>
2021-11-29 10:33:09 +0000

    hmm

--- toso2021_overallModulation.m
+++ toso2021_overallModulation.m
@@ -4,7 +4,7 @@ if ~exist('data','var')
 end
 
 %% contrast settings
-contrast_str = 't2';
+contrast_str = 'choices';
 contrasts = eval(contrast_str);
 contrast_set = eval([contrast_str(1:end-1),'_set']);
 n_contrasts = numel(contrast_set);
