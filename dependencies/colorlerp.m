510d048c8cf23b155dac56259db66654772f260b
flipe rodrigues <filipe.rodrigues@neuro.fchampalimaud.org>
2023-01-26 14:51:01 +0000

    decoder aesthetics

--- dependencies/colorlerp.m
+++ dependencies/colorlerp.m
@@ -3,16 +3,14 @@ function map = colorlerp(clrs,k)
     %   Detailed explanation goes here
 
     K = 1e3;
-    n = size(clrs,1);
+    [n,d] = size(clrs);
     m = K / (n - 1);
-    map = nan(k,3);
+    map = nan(k,d);
     for ii = 1 : n - 1
         idcs = (1 : m) + (ii - 1) * m;
-        map(idcs,:) = [...
-            linspace(clrs(ii,1),clrs(ii+1,1),m); ...
-            linspace(clrs(ii,2),clrs(ii+1,2),m); ...
-            linspace(clrs(ii,3),clrs(ii+1,3),m); ...
-            ]';
+        map(idcs,:) = cell2mat(...
+            arrayfun(@(dim)linspace(clrs(ii,d),clrs(ii+1,d),m)',1:d,...
+            'uniformoutput',false));
     end
     idcs2keep = round(linspace(1,K,k));
     idcs2keep = min(max(idcs2keep,1),K);
