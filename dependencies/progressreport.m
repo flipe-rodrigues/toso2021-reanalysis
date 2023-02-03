131b8492ab1628487d3a284d8d34fbe979c905e3
flipe rodrigues <filipe.rodrigues@neuro.fchampalimaud.org>
2023-02-03 09:22:52 +0000

    v2 decoder

--- dependencies/progressreport.m
+++ dependencies/progressreport.m
@@ -1,30 +1,34 @@
-function progressreport( iter, n_iter, operation, bar_len )
+function progressreport( iter, n_iter, message, bar_len )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-
-if nargin < 4
-    bar_len = 30;
-end
-
-progress = floor(iter / n_iter * 100);
-if iter > 1
-    for jj = 1 : (length(operation) + bar_len + 8)
-        fprintf('\b');
+    if nargin < 4
+        bar_len = 30;
     end
-end
-fprintf('|');
-for jj = 1 : bar_len
-    if progress/(100/bar_len) >= jj
-        fprintf('#');
+    persistent progress;
+    if isempty(progress)
+        progress = -1;
+    elseif progress ~= floor(iter / n_iter * 100)
+        progress = floor(iter / n_iter * 100);
     else
-        fprintf('-');
+       return; 
     end
-end
-fprintf('| ');
-if progress == 100
-    fprintf([operation, ' ... done.\n']);
-else
-    fprintf([operation, ' ... ']);
-end
-
+    if iter > 1
+        for jj = 1 : (length(message) + bar_len + 8)
+            fprintf('\b');
+        end
+    end
+    fprintf('|');
+    for jj = 1 : bar_len
+        if progress/(100/bar_len) >= jj
+            fprintf('#');
+        else
+            fprintf('-');
+        end
+    end
+    fprintf('| ');
+    if progress == 100
+        fprintf([message, ' ... done.\n']);
+    else
+        fprintf([message, ' ... ']);
+    end
 end
