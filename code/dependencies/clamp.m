fb93d347346247fd6220d6882fd38d9c379ca113
flipe rodrigues <filipe.rodrigues@neuro.fchampalimaud.org>
2023-11-17 13:42:48 +0000

    missing dependencies

---
+++ code/dependencies/clamp.m
@@ -0,0 +1,6 @@
+function y = clamp(x,a,b)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+y = min(max(x,a),b);
+end
+
---
+++ code/dependencies/normalize01.m
@@ -0,0 +1,9 @@
+function z = normalize01(x,d)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+if nargin == 1
+    d = 1;
+end
+z = (x - min(x,[],d)) ./ range(x,d);
+end
+
