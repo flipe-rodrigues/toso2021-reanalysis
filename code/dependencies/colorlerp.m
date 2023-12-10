1ae9c4ee1c5c8b44328b937b84b26be5a1d056c8
flipe rodrigues <filipe.rodrigues@neuro.fchampalimaud.org>
2023-12-10 22:15:43 +0000

    s5 (almost...)

--- code/dependencies/colorlerp.m
+++ code/dependencies/colorlerp.m
@@ -3,7 +3,7 @@ function map = colorlerp(clrs,k)
     %   Detailed explanation goes here
     K = 1e3;
     n = size(clrs,1);
-    m = K / (n - 1);
+    m = floor(K / (n - 1));
     map = nan(k,3);
     for ii = 1 : n - 1
         idcs = (1 : m) + (ii - 1) * m;
@@ -13,6 +13,7 @@ function map = colorlerp(clrs,k)
             linspace(clrs(ii,3),clrs(ii+1,3),m); ...
             ]';
     end
+    map(end,:) = clrs(end,:); 
     idcs2keep = round(linspace(1,K,k));
     idcs2keep = min(max(idcs2keep,1),K);
     map = map(idcs2keep,:);
